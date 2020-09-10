e5fda9e8195986a18698007ea8331b84b53f6f04
EmilyChristy <emily@brainstormcreative.co.uk>
2020-09-10 10:11:32 +0100

    two pages, basic nav in place

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { StyleSheet, Text, Button, Alert, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headline}>This is a headline</Text>
+        <Text style={styles.body}>a text line goes here</Text>
+        <Text style={styles.body}>another text line goes here</Text>
+        <Text style={styles.body}>and the last text line goes here</Text>
+        <Button
+          title="Next >"
+          onPress={() => this.props.navigation.navigate("Details")}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: "Home",
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ccc",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    padding: 10,
+  },
+  headline: {
+    color: "#000",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 20,
+    marginBottom: 20,
   },
 });
